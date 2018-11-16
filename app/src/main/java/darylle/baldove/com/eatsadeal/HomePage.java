0954b8286eb426509aafc231ac9bd42befae53d7
JustinaSantos <2015081396@ust-ics.mygbiz.com>
2018-11-17 02:36:36 +0800

    11/17/18 - Added DBHelper & Animations

--- app/src/main/java/darylle/baldove/com/eatsadeal/HomePage.java
+++ app/src/main/java/darylle/baldove/com/eatsadeal/HomePage.java
@@ -1,16 +1,30 @@
 package darylle.baldove.com.eatsadeal;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class HomePage extends AppCompatActivity {
+
+    Cursor table;
+    DatabaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
 
         Spinner s = (Spinner) findViewById(R.id.place);
         ArrayAdapter<CharSequence> a = ArrayAdapter.createFromResource(this, R.array.place, android.R.layout.simple_spinner_item);
@@ -27,5 +41,37 @@ public class HomePage extends AppCompatActivity {
         ada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spi.setAdapter(ada);
 
+    }
+
+
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    public class ProgressBarAnimation extends Animation {
+        private ProgressBar progressBar;
+        private float from;
+        private float  to;
+
+        public ProgressBarAnimation(ProgressBar progressBar, float from, float to) {
+            super();
+            this.progressBar = progressBar;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            super.applyTransformation(interpolatedTime, t);
+            float value = from + (to - from) * interpolatedTime;
+            progressBar.setProgress((int) value);
+        }
+
+    }
+
+    public void generate(View v)
+    {
+         Intent i = new Intent(this, ResultPage.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/darylle/baldove/com/eatsadeal/MainActivity.java
+++ app/src/main/java/darylle/baldove/com/eatsadeal/MainActivity.java
@@ -4,13 +4,34 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
+
     private static int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.text_view);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressLoader();
+
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
@@ -19,5 +40,11 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    public void progressLoader(){
+        ProgressBarLoader anim = new ProgressBarLoader(this, progressBar, textView, 0f, 100f);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
     }
 }
